0af007e0d4843c7d17023afde6ea7afee308c55a
kabilan <bkabilan23@gmail.com>
2025-02-14 13:53:27 +0530

    learning js

---
+++ Pre-requisites/Learn_Js/Declaration.js
@@ -0,0 +1,34 @@
+array=[1,2,3,3,4];
+console.log(array[0])
+
+//1st Type of object declaration
+object = {
+    "name" : "Danieal",
+    "age" : 19,
+    "skills" : ["Programming", "Designing"]
+}
+console.log(object)
+console.log(object.name)
+console.log(object["age"])
+//2nd Type of object declaration
+object2 = {}
+object2["name"] = "Darwin"
+object2["age"] = 20
+object2["skills"] = ["Programming", "Designing"]
+
+console.log(object2)
+//3rd Type of object declaration
+
+object3= new Object();
+object3.name = "HI"
+console.log(object3);
+
+//set 
+set = new Set(["hello"])
+set.add("hello")
+set.add("hi")
+console.log(set)
+
+// shift operators
+console.log(-8>>3)
+
